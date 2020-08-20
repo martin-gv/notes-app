2eb14c599d84853ee1689d2d160766eb18f768ed
Martin Guardado <martinguardado@gmail.com>
2020-08-19 21:58:48 -0600

    remove old new note modal code

--- src/components/Sidebar/NewNote.js
+++ src/components/Sidebar/NewNote.js
@@ -1,59 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { useStoreState, useStoreActions } from 'easy-peasy';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
 
 import NewNoteButton from './NewNote/NewNoteButton';
-import NoteModal from '../Note/NoteModal/NoteModal';
-import useCloseMobileMenu from '../../hooks/useCloseMobileMenu';
 
 function NewNote({ toggleModal }) {
-  const [show, setShow] = useState(false);
-
-  const createNewNote = useStoreActions(
-    (actions) => actions.notes.createNewNote,
-  );
-  const notes = useStoreState((state) => state.notes.notes);
-  const [newNoteId, setNewNoteId] = useState(null);
-
-  const history = useHistory();
-
-  const setSearch = useStoreActions((actions) => actions.setSearch);
-  const closeMobileMenu = useCloseMobileMenu();
-
-  // function toggleModal() {
-  //   createNewNote();
-  //   setShow((prev) => !prev);
-  //   setSearch('');
-  //   history.push('/');
-  //   closeMobileMenu();
-  // }
-
-  const removeNote = useStoreActions((actions) => actions.notes.remove);
-  function deleteClickHandler(e) {
-    e.stopPropagation();
-    removeNote(newNoteId);
-  }
-
-  useEffect(() => {
-    if (show) setNewNoteId(notes[0].id);
-  }, [show, notes]);
-
-  const newNote = newNoteId
-    ? notes.find((note) => note.id === newNoteId)
-    : null;
-
-  return (
-    <>
-      <NewNoteButton onClick={toggleModal} />
-      <NoteModal
-        show={show}
-        close={() => setShow(false)}
-        note={newNote}
-        isNewNote={true}
-        deleteClick={deleteClickHandler}
-      />
-    </>
-  );
+  return <NewNoteButton onClick={toggleModal} />;
 }
 
 export default NewNote;
