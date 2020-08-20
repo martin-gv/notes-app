bac4e37eb68a024318cf8c427c8cc3f1e5459942
Martin Guardado <martinguardado@gmail.com>
2020-08-19 20:17:16 -0600

    prettier styles

--- src/components/Layout/Main/Sidebar/Sidebar.js
+++ src/components/Layout/Main/Sidebar/Sidebar.js
@@ -1,20 +1,20 @@
-import React from "react";
-import styles from "./Sidebar.module.scss";
-import { useStoreState } from "easy-peasy";
-import cx from "classnames";
+import React from 'react';
+import styles from './Sidebar.module.scss';
+import { useStoreState } from 'easy-peasy';
+import cx from 'classnames';
 
-import Divider from "../../../Shared/Divider";
-import AllNotes from "../../../Sidebar/AllNotes";
-import NewNote from "../../../Sidebar/NewNote";
-import TagSection from "../../../Sidebar/TagSection";
-import Trash from "../../../Sidebar/Trash";
+import Divider from '../../../Shared/Divider';
+import AllNotes from '../../../Sidebar/AllNotes';
+import NewNote from '../../../Sidebar/NewNote';
+import TagSection from '../../../Sidebar/TagSection';
+import Trash from '../../../Sidebar/Trash';
 
 function Sidebar() {
-  const isMobileMenuOpen = useStoreState(state => state.isMobileMenuOpen);
+  const isMobileMenuOpen = useStoreState((state) => state.isMobileMenuOpen);
 
   const joinedClasses = cx(
     styles.Sidebar,
-    isMobileMenuOpen && styles.ShowMobileMenu
+    isMobileMenuOpen && styles.ShowMobileMenu,
   );
 
   return (
